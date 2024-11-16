66fc5a33a46db47ec16242f5eb355409bce4889c
baby-sal <sally.facebook123@gmail.com>
2024-11-16 17:02:42 +0000

    Adding functions and fine-tuning the classes on db_utils.py

--- Logic/Connector/db_utils.py
+++ Logic/Connector/db_utils.py
@@ -5,6 +5,8 @@ need to install: pip install mysql-connector-python
 import mysql.connector
 from config import USER, PASSWORD, HOST, DATABASE
 
+##Database class
+
 class DbClass(object):
 
     connection = None
@@ -24,12 +26,22 @@ class DbClass(object):
             else:
                 print("Successfully connected to the database.")
 
-    def query(self,sql_query, params=None):
+    def get_query(self,sql_query, params=None):
         curs = self.connection.cursor()
         try:
             curs.execute(sql_query,params)
-            result = curs.fetchall()
-            return result
+            return curs.fetchall()
+        except Exception as an_error:
+            print(f"Error executing query {sql_query}: {an_error}")
+            return None
+        finally:
+            curs.close()
+
+    def update_query(self, sql_query, params=None):
+        curs = self.connection.cursor()
+        try:
+            curs.execute(sql_query, params)
+            self.connection.commit()
         except Exception as an_error:
             print(f"Error executing query {sql_query}: {an_error}")
             return None
@@ -41,13 +53,25 @@ class DbClass(object):
             self.connection.close()
             return f"Disconnected from {self.database}"
 
+
+## Functions
+
 def get_top_ten():
     database = DbClass(HOST, USER, PASSWORD, DATABASE)
     try:
         database.db_connect()
         sql_query = "SELECT * FROM high_scores"
-        result = database.query(sql_query)
+        result = database.get_query(sql_query)
         return result
+    finally:
+        database.db_disconnect()
+
+def add_new_score(nickname, score):
+    database = DbClass(HOST, USER, PASSWORD, DATABASE)
+    try:
+        database.db_connect()
+        sql_query = f"UPDATE high_scores SET date = CURDATE(), nickname = {nickname}, score = {score}"
+        database.update_query(sql_query, nickname, score)
     finally:
         database.db_disconnect()
 
