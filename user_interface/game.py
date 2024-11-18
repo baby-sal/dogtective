ea74b2da2f02d9dedf452d808e0482df4ada92ce
Melanie Clark <melaniefclark@gmail.com>
2024-11-18 16:45:45 +0000

    game.py collision removed, game_loop refined

--- user_interface/game.py
+++ user_interface/game.py
@@ -1,7 +1,7 @@
 # game render and loop
 
 import pygame
-
+import sys
 
 # Game window set-up
 def initial_game_window():
@@ -13,38 +13,25 @@ def initial_game_window():
 
     game_display = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption("Dogtective")
+    clock = pygame.time.Clock()
 
     dogtective = pygame.image.load('../logic/assets/images/characters/dogtective_image.png')
     dogtective_coords = [10, 700]
     game_display.blit(dogtective, dogtective_coords)
     pygame.display.update()
 
-    clock = pygame.time.Clock()
 
-    return dogtective_coords, dogtective, game_display
-
-# This is incorrect and needs to be deleted or re-written!
-def movement(dogtective_coords, dogtective, game_display):
-    collision = False
-    while not collision:
+# Game loop: Keeps window open until quit
+def game_loop():
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    dogtective_coords += [-10, 0]
-                elif event.key == pygame.K_RIGHT:
-                    dogtective_coords += [10, 0]
-                elif event.key == pygame.K_UP:
-                    dogtective_coords += [0, -10]
-                elif event.key == pygame.K_DOWN:
-                    dogtective_coords += [0, 10]
-        game_display.blit(dogtective, dogtective_coords)
+                sys.exit()
 
 
 def run():
-    dogtective_coords, dogtective, game_display = initial_game_window()
-    movement(dogtective_coords, dogtective, game_display)
+    initial_game_window()
+    game_loop()
 
 
 if __name__ == '__main__':
