b18ea1d10cc0d23439ca85ef6a8bc44dcd67b013
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-11-20 15:35:11 +0000

    creating Obstacle class

--- logic/components/enviromental.py
+++ logic/components/enviromental.py
@@ -1,13 +1,35 @@
 from entity import Entity
 
 
+# this will need more functionality to justify its existence at some point
 class Environmental(Entity):
 
     def __init__(self, name):
         super().__init__(name)
 
+    def draw(self):
+        pass
+
     def __str__(self):
         return f"Name: {self.name}"
 
+
+class Obstacle(Environmental):
+
+    def __init__(self, name, damage, speed):
+        super().__init__(name)
+        self.damage = damage
+        self.speed = speed
+
     def draw(self):
         pass
+
+    def __str__(self):
+        return f"Name: {self.name}, Damage:{self.speed}, Speed: {self.speed}, "
+
+
+if __name__ == "__main__":
+    tree = Environmental("tree")
+    print(tree)
+    car = Obstacle("car", 10, 2)
+    print(car)
