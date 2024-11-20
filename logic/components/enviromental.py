944e8e13723bdf3dd3eb0e18176b1fe458fa54a1
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-11-20 16:05:09 +0000

    creating an instance of an obstacle that moves across the screen with placeholder image

--- logic/components/enviromental.py
+++ logic/components/enviromental.py
@@ -1,4 +1,5 @@
-from entity import Entity
+from logic.components.entity import Entity
+import pygame
 
 
 # this will need more functionality to justify its existence at some point
@@ -16,13 +17,23 @@ class Environmental(Entity):
 
 class Obstacle(Environmental):
 
-    def __init__(self, name, damage, speed):
+    def __init__(self, name, damage, speed, direction, image, x, y, scale):
         super().__init__(name)
         self.damage = damage
         self.speed = speed
+        self.direction = direction  # 1 if right, -1 if left
 
-    def draw(self):
-        pass
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        self.rect.x += self.direction * self.speed
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def __str__(self):
         return f"Name: {self.name}, Damage:{self.speed}, Speed: {self.speed}, "
--- user_interface/game.py
+++ user_interface/game.py
@@ -2,37 +2,51 @@
 
 import pygame
 import sys
+from logic.components.enviromental import Obstacle
 
 
 # Game window set-up
-def initial_game_window():
-    # Initialise pygame
-    pygame.init()
 
-    display_width = 1200
-    display_height = 780
+# Initialise pygame
+pygame.init()
 
-    game_display = pygame.display.set_mode((display_width, display_height))
-    pygame.display.set_caption("Dogtective")
-    clock = pygame.time.Clock()
+display_width = 1200
+display_height = 780
 
-    dogtective = pygame.image.load('../logic/assets/images/characters/dogtective_image.png')
-    dogtective_coords = [10, 700]
-    game_display.blit(dogtective, dogtective_coords)
-    pygame.display.update()
+game_display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("Dogtective")
+clock = pygame.time.Clock()
+
+dogtective = pygame.image.load('../logic/assets/images/characters/dogtective_image.png')
+dogtective_coords = [10, 700]
 
 
 # Game loop: Keeps window open until quit
 def game_loop():
+
+    car_image = pygame.image.load('../logic/assets/images/obstacles/car.png').convert_alpha()
+    car = Obstacle("car", 10, 10, -1, car_image, 900, 0, 0.25)
+
+    game_display.blit(dogtective, dogtective_coords)
+    car.draw(game_display)
+    pygame.display.update()
+
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()  # releases pygame resources
                 sys.exit()
 
+        game_display.fill((0, 0, 0))
+
+        car.update()
+        car.draw(game_display)
+        pygame.display.update()
+        clock.tick(10)
+
 
 def run():
-    initial_game_window()
     game_loop()
 
 
