2ebb7909f73ed38c49d0f6825400a29b235610d5
esterwalf <estellewalford@gmail.com>
2024-11-21 08:23:02 +0000

    updated hurt image

--- Pygame_ideas/crossy_roads.py
+++ Pygame_ideas/crossy_roads.py
@@ -171,7 +171,7 @@ class Explosion(object):
         self.costume = 1
         self.width = 140
         self.height = 140
-        self.image = p.image.load('explosion' + str(self.costume) + '.png')#update explosion name once image decided on
+        self.image = p.image.load('../logic/characters/dogtective_sprite/Hurt.png')
         self.image = p.transform.scale(self.image, (self.width, self.height))#updates the size of the image
 
     def explode(self, x, y):
