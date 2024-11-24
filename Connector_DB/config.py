8fb1103511aabd21120f433b4fe35f182f845066
baby-sal <sally.facebook123@gmail.com>
2024-11-24 15:51:58 +0000

    app and config and updating the git ignore

--- Connector_DB/config.py
+++ Connector_DB/config.py
@@ -1,5 +1,5 @@
 #use as a git ignore file, would normally be a .env file
-HOST = "localhost"
-USER = "root"
-PASSWORD = "root"
-db_name="game_init"
+USER = ""
+PASSWORD = ""
+HOST = ""
+DATABASE = ""
