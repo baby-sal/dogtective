456ff458901cb521b2ca7c0ba02787d8ab7cab45
Melanie Clark <melaniefclark@gmail.com>
2024-11-24 12:04:16 +0000

    car_right, car_up and car_down images added.

--- user_interface/game.py
+++ user_interface/game.py
@@ -20,13 +20,19 @@ class GameRunner:
         self.clock = pygame.time.Clock()
 
         # to be replaced with instance of Player/Character
-        dogtective = pygame.image.load('../logic/assets/images/characters/dogtective_image.png')
+        dogtective = pygame.image.load('../logic/assets/images/characters/dogtective_sprite/walk.png')
         dogtective_coords = [10, 700]
 
-        car_img = pygame.image.load('../logic/assets/images/obstacles/car.png').convert_alpha()
-        car1 = Obstacle("car1", car_img, 0, 0, 0.2, 10, 10, [1, 0])
-        car2 = Obstacle("car2", car_img, 1000, 100, 0.2, 10, 10, [-1, 0])
-        self.cars = [car1, car2]
+        # may need to refactored into its own function
+        car_right_img = pygame.image.load('../logic/assets/images/obstacles/car/car_right_img.png').convert_alpha()
+        car_left_img = pygame.image.load('../logic/assets/images/obstacles/car/car_left_img.png').convert_alpha()
+        car_up_img = pygame.image.load('../logic/assets/images/obstacles/car/car_up_img.png').convert_alpha()
+        car_down_img = pygame.image.load('../logic/assets/images/obstacles/car/car_down_img.png').convert_alpha()
+        car_right = Obstacle("car_right", car_right_img, 0, 0, 0.2, 10, 10, [1, 0])
+        car_left = Obstacle("car_left", car_left_img, 1000, 100, 0.2, 10, 10, [-1, 0])
+        car_up = Obstacle("car_up", car_up_img, 100, 700, 0.2, 10, 10, [0, -1])
+        car_down = Obstacle("car_up", car_down_img, 500, 0, 0.2, 10, 10, [0, 1])
+        self.cars = [car_left, car_right, car_up, car_down]
 
         self.game_display.blit(dogtective, dogtective_coords)
         pygame.display.update()
