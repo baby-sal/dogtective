863e0ecf0a7ad0dd9c25c1501d1c0796ffb39c92
AversionToDeepWater <imanabdelgani@gmail.com>
2024-11-25 19:27:35 +0000

    Had Mel help me with this, as I was struggling to translate the sprite animation to movement with user input.

--- logic/assets/get_sprite_image.py
+++ logic/assets/get_sprite_image.py
@@ -1,7 +1,23 @@
 import pygame
 
-
 class SpriteSheet():
+    def __init__(self, image):
+        self.sheet = image
+
+    def get_image(self, frame, width, height, scale, colour):
+        # first need to make a blank surface
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0, 0),((frame * width), 0, width, height))  # has taken the whole sprite sheet and only taken part of it
+        # scale image and resize to make bigger
+        image = pygame.transform.scale(image, (width * scale, height * scale))  # make sprite bigger or smaller
+        image.set_colorkey(colour)  # transparent background instead of black background
+        return image
+
+
+'''
+Tried to incorporate everything into a SpriteSheet class but I haven't done that properly so commenting it out
+
+class SpriteSheet:
     def __init__(self, image):
         self.sheet = image
         self.frame = 0
@@ -19,10 +35,10 @@ class SpriteSheet():
         image.set_colorkey(colour)  # transparent background instead of black background
         return image
 
-    def sprite_motion(self, link, steps):
+    def sprite_motion(self,link , steps):
         # arguments link to sprite sheet, with step number depending on animation
         sprite_sheet_image = pygame.image.load(link).convert_alpha()
-        sprite_sheet = self.get_image(sprite_sheet_image) #do I put more arguments here?
+        # sprite_sheet = self.get_image(sprite_sheet_image) #do I put more arguments here?
 
         black = (0, 0, 0)  # background of image
 
@@ -33,13 +49,18 @@ class SpriteSheet():
             image = self.get_image(x, 48, 48, 2, black)
             self.animation_list.append(image)
 
+
     def update_animation(self):
         current_time = pygame.time.get_ticks()
         if current_time - self.last_update >= self.animation_cooldown:
             self.frame += 1
-            last_update = current_time
+            self.last_update = current_time
             if self.frame >= len(self.animation_list):  # loops back to first image in sprite sheet
                 frame = 0
+
+
     def on_screen(self, screen, x, y):
         #assuming the character goes in the middle of a 1200x720 screen, x = 600 and y = 360
         screen.blit(self.animation_list[self.frame], (x, y))
+
+'''
--- logic/assets/load_sprite.py
+++ logic/assets/load_sprite.py
@@ -1,5 +1,5 @@
 import pygame
-from get_sprite_image import SpriteSheet
+import get_sprite_image
 
 pygame.init()
 screen_width = 1200
@@ -7,7 +7,7 @@ screen_height = 720
 
 #temporary screen
 screen = pygame.display.set_mode((screen_width,screen_height))
-BG = (50, 50, 50) #screen bg colour
+background_colour = (50, 50, 50) #screen bg colour
 
 '''sprite image loops for character sprites 
 attack = 4 images - might not need attack animation
@@ -17,47 +17,31 @@ idle = 4 images
 walk = 6 images
 '''
 
-# #add to class function?
-# def sprite_motion(link, steps:int):
-#     # arguments link to sprite sheet, with step number depending on animation
-#     sprite_sheet_image = pygame.image.load(link).convert_alpha()
-#     sprite_sheet = SpriteSheet(sprite_sheet_image)
-#
-#     black = (0,0,0) #background of image
-#
-#     #Create animation list i.e., list containing each animation frame
-#     animation_list = []
-#
-#     last_update = pygame.time.get_ticks() # need this to refresh animation, see line x
-#     animation_cooldown = 75 #in milliseconds, how long to cycle through each frame
-#     frame = 0
-#
-#     for x in range(steps):
-#         #sprite sheet images are 48x48 pixels, therefore increase size by factor of 2 for 96x96
-#         #this can be changed
-#         animation_list.append(sprite_sheet.get_image(x,48,48,2,black))
-#
-#     current_time = pygame.time.get_ticks()
-#     if current_time - last_update >= animation_cooldown:
-#         frame += 1
-#         last_update = current_time
-#         if frame >= len(animation_list): #loops back to first image in sprite sheet
-#             frame = 0
-#
-#
-#     # #show frame image
-#     return screen.blit(animation_list[frame],(0, 0))
 
-    # need tp blit image from this onto surface, and animate motion
+#add to class function?
+def sprite_motion(link, steps: int):
+    # arguments link to sprite sheet, with step number depending on animation
+    sprite_sheet_image = pygame.image.load(link).convert_alpha()
+    sprite_sheet = get_sprite_image.SpriteSheet(sprite_sheet_image)
 
-dogtective_walk_left = SpriteSheet("logic/assets/images/characters/dogtective_sprite/Walk.png", 6)
-dogtective_walk_right = pygame.transform.flip(dogtective_walk_left, True, False) #flip animation on y axis?
-dogtective_death = sprite_motion("logic/assets/images/characters/dogtective_sprite/Death.png", 4)
-dogtective_idle = sprite_motion("logic/assets/images/characters/dogtective_sprite/Idle.png", 4)
-dogtective_hurt = sprite_motion("logic/assets/images/characters/dogtective_sprite/Hurt.png", 2)
+    black = (0, 0, 0)  # background of image
 
+    #Create animation list i.e., list containing each animation frame
+    animation_list = []
 
-current_direction = "left"
+    for x in range(steps):
+        #sprite sheet images are 48x48 pixels, therefore increase size by factor of 2 for 96x96
+        #this can be changed
+        animation_list.append(sprite_sheet.get_image(x, 48, 48, 2, black))
+    return animation_list
+
+dogtective_walk_right = sprite_motion('../assets/images/characters/dogtective_sprite/Walk.png', 6)
+dogtective_walk_left = [pygame.transform.flip(frame, True, False).convert_alpha() for frame in dogtective_walk_right]
+
+# uncomment when required
+# dogtective_death = sprite_motion("logic/assets/images/characters/dogtective_sprite/Death.png", 4)
+# dogtective_idle = sprite_motion("logic/assets/images/characters/dogtective_sprite/Idle.png", 4)
+# dogtective_hurt = sprite_motion("logic/assets/images/characters/dogtective_sprite/Hurt.png", 2)
 
 '''MOVING SPRITES 
 There are two options:
@@ -71,27 +55,63 @@ This would mean that the sprite would stay centre in the screen.
 
 '''
 
+# Starting frame, position and speed
+first_frame = dogtective_walk_right[0]
+dogtective_x = screen_width // 2 - first_frame.get_width() // 2 # center of x_axis
+dogtective_y = screen_height // 2 - first_frame.get_height() // 2 # center of y_axis
+dogtective_speed = 1.5  # Speed of movement
+
+# Animation frames
+last_update = pygame.time.get_ticks()  # need this to refresh animation, see line x
+animation_cooldown = 75  # in milliseconds, how long to cycle through each frame
+frame = 0
 
 run = True
 while run:
-    screen.fill(BG) # temporary screen
-    #event handlers
+    screen.fill(background_colour)  # temporary screen
+
+    # event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    # so you can continually press the key to make sprite move
     keys = pygame.key.get_pressed()
+    direction = None  # None = Dog doesn't animate in starting position, change to a direction i.e. right if you want dog to animate on the spot
+
+    if keys[pygame.K_LEFT]:
+        dogtective_x -= dogtective_speed  # speed is required to move the dog along the x-axis
+        direction = 'left'
     if keys[pygame.K_RIGHT]:
-        current_direction = "right"
-        dogtective_walk_right
+        dogtective_x += dogtective_speed
+        direction = 'right'
+    if keys[pygame.K_UP]:
+        dogtective_y -= dogtective_speed  # speed is required to move the dog along the y-axis
+        direction = 'up'
+    if keys[pygame.K_DOWN]:
+        dogtective_y += dogtective_speed
+        direction = 'down'
 
+    # Handle frame updates for animation
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_cooldown:
+        frame += 1
+        last_update = current_time
+        if frame >= len(dogtective_walk_left):  #loops back to first image in sprite sheet
+            frame = 0
 
+    # Get the correct frame based on direction
+    if direction == 'left':
+        current_frame = dogtective_walk_left[frame]
+    elif direction == 'right':
+        current_frame = dogtective_walk_right[frame]
+    else:
+        current_frame = dogtective_walk_right[0]  # Default to walking right if no direction is set
 
+    # Draw the sprite at the updated position
+    screen.blit(current_frame, (dogtective_x, dogtective_y))
+
+    # Update the display
     pygame.display.update()
-
-
-
 
 if __name__ == "__main__":
     pass
