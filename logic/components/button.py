4c5f187424e17c9a38f010777195632900c875ba
baby-sal <sally.facebook123@gmail.com>
2024-11-26 21:14:50 +0000

    button class

---
+++ logic/components/button.py
@@ -0,0 +1,13 @@
+class Button():
+	def __init__(self, image, pos_x, pos_y):
+		self.image = image
+		self.pos_x = pos_x
+		self.pos_y = pos_y
+		self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y)
+
+	def update_button(self, screen):
+		screen.blit(self.image, self.rect)
+
+	def check_input(self, pos):
+		if pos[0] in range(self.rect.left, self.rect.right) and pos[1] in range(self.rect.top, self.rect.bottom):
+			return True
