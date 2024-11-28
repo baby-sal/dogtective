768cea2c6306affafc06fc48845b49f13bd0cf4a
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-11-28 09:43:38 +0000

    importing game_config.py to game.py to set screen dimensions and fps

--- user_interface/game.py
+++ user_interface/game.py
@@ -4,17 +4,18 @@ import pygame
 import sys
 from logic.components.environmental import Obstacle
 from logic.components.character import Character
+import user_interface.game_config as config
 
 
 class GameRunner:
     # Game window set-up
 
-    def __init__(self, dis_width, dis_height):
+    def __init__(self):
         # Initialise pygame
         pygame.init()
 
-        self.dis_width = dis_width
-        self.dis_height = dis_height
+        self.dis_width = config.WIDTH
+        self.dis_height = config.HEIGHT
 
         self.game_display = pygame.display.set_mode((self.dis_width, self.dis_height))
         pygame.display.set_caption("Dogtective")
@@ -25,12 +26,11 @@ class GameRunner:
         self.dog_group.add(self.dog)
 
         car_img = pygame.image.load('../logic/assets/images/obstacles/car.png').convert_alpha()
-        car1 = Obstacle("car1", car_img, 190, 0, 0.2, 1, 10, [1, 0])
-        car2 = Obstacle("car2", car_img, 460, 100, 0.2, 2, 10, [-1, 0])
+        car1 = Obstacle("car1", car_img, 190, 0, 0.2, 1, 3, [1, 0])
+        car2 = Obstacle("car2", car_img, 460, 100, 0.2, 2, 5, [-1, 0])
         self.car_group = pygame.sprite.Group()
         self.car_group.add(car1, car2)
 
-        # self.game_display.blit(dogtective, dogtective_coords)
         pygame.display.update()
 
     def render_all(self, *groups):
@@ -58,14 +58,14 @@ class GameRunner:
                 self.render_dog(self.car_group)
 
                 pygame.display.update()
-                self.clock.tick(20)
+                self.clock.tick(config.FPS)
             else:
                 # game over screen here
                 run = False
 
 
 def run():
-    game = GameRunner(1200, 720)
+    game = GameRunner()
     game.game_loop()
     pygame.quit()
     sys.exit()
