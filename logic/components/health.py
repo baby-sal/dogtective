098954cd77a77117a0260d41ec6c71fd2097a81e
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-11-29 15:18:55 +0000

    refactoring files for health class and bone sprite into main game folders

--- logic/components/health.py
+++ logic/components/health.py
@@ -1,21 +1,23 @@
 import pygame
 from user_interface.game_config import HEIGHT, WIDTH
 
+
 class Health(pygame.sprite.Sprite):
     def __init__(self, number):
         super().__init__()
         self.number = number
 
         self.visible = True
-        self.x = 30#opposite end of the screen
+        self.x = 30 # opposite end of the screen
+        self.y = 30
         self.width = 50
         self.height = 50
 
-        self.image = pygame.image.load('../Pygame_ideas/src/Bone.png').convert_alpha()#update name once image loaded     
+        self.image = pygame.image.load('../assets/images/objects/Bone.png').convert_alpha()  # update name once image loaded
         self.image = pygame.transform.rotate(self.image, -25)
-        self.y = 30
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
+
     def draw_bones(self, screen): 
             for i in range(self.number): screen.blit(self.image, (self.x + i * (self.width + -30), self.y))
             
