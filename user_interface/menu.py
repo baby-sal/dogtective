d9c479d8865bb762b3d4f27f1bd4113fd94f6cbc
esterwalf <estellewalford@gmail.com>
2024-11-30 20:58:59 +0000

    changed link from personal desktop to where most of our files are saved on github

--- user_interface/menu.py
+++ user_interface/menu.py
@@ -10,11 +10,11 @@ pygame.init()
 
 display = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Dogtective: Main Menu")
-background = pygame.image.load("/Users/sallydavies/Desktop/PycharmProjects/CFGDegree-GroupProjectTeam5/logic/assets/images/menu/city_backgroud.png").convert()
+background = pygame.image.load("../logic/assets/logic/assets/images/menu/city_backgroud.png").convert()
 background = pygame.transform.smoothscale(background, display.get_size())
 
 def pixel_font(size):
-    return pygame.font.Font("/Users/sallydavies/Desktop/PycharmProjects/CFGDegree-GroupProjectTeam5/logic/assets/StayPixelRegular.ttf", size)
+    return pygame.font.Font("../logic/assets/StayPixelRegular.ttf", size)
 
 def text_blit(text, size, colour, rect_pos_x, rect_pos_y):
     text = pixel_font(size).render(text,True, colour)
@@ -28,7 +28,7 @@ def credit_blit(text, pos_y):
 
 def dog_image(pos_x,pos_y):
     smol_dog_pic = pygame.image.load(
-        "/Users/sallydavies/Desktop/PycharmProjects/CFGDegree-GroupProjectTeam5/logic/assets/images/characters/dogtective_sprite/Walk.png").convert()
+        "../logic/assets/logic/assets/images/characters/dogtective_sprite/Walk.png").convert()
     rect_dog = smol_dog_pic.get_rect(center=(pos_x, pos_y))
     display.blit(smol_dog_pic, rect_dog)
 
