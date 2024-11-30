9546b3a3437c7ea9314b3911d91d0d9c98916969
esterwalf <estellewalford@gmail.com>
2024-11-30 22:31:07 +0000

    moved ball logic to lost toy file for incoporating into the game later

---
+++ logic/components/lost_toy.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+
+
+ball_img = pygame.image.load(
+            '../logic/assets/images/objects/ball.png').convert_alpha()
+        ball = Environmental("ball", ball_img, self.dis_width * 0.96,
+                             self.dis_height * 0.95, 0.1)
+        self.final_screen_ball_group = pygame.sprite.Group()
+        self.final_screen_ball_group.add(ball)
+
+
+def render_final_screen_ball(self, ball):
+    self.final_screen_ball_group.draw(self.game_display)
+    self.final_screen_ball_group.update(ball)
+
+
+ball_active = False
+
+self.render_final_screen_ball(self.final_screen_ball_group)
