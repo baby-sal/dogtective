cf2cce2d6f2546f466a1eb83a4be24017a538ec3
esterwalf <estellewalford@gmail.com>
2024-12-01 17:59:03 +0000

    fixed errors within end screen

--- logic/screens/end_screen.py
+++ logic/screens/end_screen.py
@@ -4,46 +4,41 @@ from user_interface.game_config import WIDTH, HEIGHT
 class EndScreen(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.img1 = pygame.image.load('Scene.png')#Update with scenery images
-        self.img2 = pygame.image.load('You Win.png')#shows when a player has won, update png once image has been confirmed
-        self.img3 = pygame.image.load('You lose.png')# shows when a player has lost, update png once image has been confirmed
+        self.img1 = pygame.image.load('Scene.png') # Update with scenery images
+        self.img2 = pygame.image.load('You Win.png') # Shows when a player has won, update png once image has been confirmed
+        self.img3 = pygame.image.load('You lose.png') # Shows when a player has lost, update png once image has been confirmed
 
         self.img1 = pygame.transform.scale(self.img1, (WIDTH, HEIGHT))
         self.img2 = pygame.transform.scale(self.img2, (WIDTH, HEIGHT))
         self.img3 = pygame.transform.scale(self.img3, (WIDTH, HEIGHT))
 
         self.image = self.img1
-        self.x = 0
-        self.y = 0
+        self.rect = self.image.get_rect(topleft=(0, 0))
 
-        self.rect = self.image.get_rect()
-
-    def update(self):
-        self.rect.topleft = (self.x, self.y)
-
-def EndScreen(n):
-    global gameOn
-
-    gameOn = False
-    """if n == 0:
-        bg.image = bg.img3 #you loose background image, need to update based on images saved
-
-    elif n == 1:
-        bg.image = bg.img2 #you win background image, need to update based on images saved"""
+    def update(self, n):
+        if n == 0:
+            self.image = self.img3 # you lose background image
+        elif n == 1:
+            self.image = self.img2 # you win background image
 
 def scoreDisplay():
-    global gameOn
-    if gameOn:
-        start_ticks = pygame.time.get_ticks()
-        elapsed_seconds = (pygame.time.get_ticks() - start_ticks) // 1000#timer for the game
-    
-    pygame.display.flip()
-    
+    start_ticks = pygame.time.get_ticks()
+
+    pygame.init()
     win = pygame.display.set_mode((WIDTH, HEIGHT))
-    bg = Screen()
-    screen_group = pygame.sprite.Group()
-    screen_group.add(bg)
-    screen_group.draw(win)
-    screen_group.update()
+    bg = EndScreen()
+    screen_group = pygame.sprite.Group(bg)
 
+    gameOn = True
+    while gameOn:
+        elapsed_seconds = (pygame.time.get_ticks() - start_ticks) // 1000 # Timer for the game
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameOn = False
+
+        screen_group.update(1) # Change 1 to 0 if the player loses
+        screen_group.draw(win)
+        pygame.display.flip()
+
+    pygame.quit()
