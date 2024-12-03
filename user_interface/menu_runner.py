448448ef39f44aa9615a1dd99b70f135d429a6ae
abbey <abbeygayle.p.dev@gmail.com>
2024-12-03 16:53:25 +0000

    spreading out the file contents

---
+++ user_interface/menu_runner.py
@@ -0,0 +1,46 @@
+import sys
+
+import pygame
+from logic.components.button import Button
+import user_interface.game_config as config
+
+
+class MenuRunner():
+
+    def menu_runner(self):
+
+        self.display = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
+        pygame.display.set_caption("Dogtective: Main Menu")
+        self.background = pygame.image.load("../logic/assets/images/menu/city_backgroud.png").convert_alpha()
+        self.background = pygame.transform.smoothscale(self.background, self.display.get_size())
+
+        while True:
+            self.display.blit(self.background, (0, 0))
+            mouse_pos_menu = pygame.mouse.get_pos()
+
+            self.text_blit("Dogtective", 200, "royalblue4", 640, 175)
+            self.text_blit("MENU", 100, "royalblue4", 640, 275)
+            self.dogtective_image(640, 600)
+
+            button_play = Button(image=None, pos_x=640, pos_y=350, font=self.pixel_font(75), colour="brown", text_in="play")
+            button_ldr = Button(image=None, pos_x=640, pos_y=425, font=self.pixel_font(75), colour="lightsalmon", text_in="LEADERBOARD")
+            button_credits = Button(image=None, pos_x=640, pos_y=500, font=self.pixel_font(75), colour="lightsalmon", text_in="CREDITS")
+
+            for button in [button_play, button_ldr, button_credits]:
+                button.update_button(self.display)
+
+            for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         pygame.quit()
+            #         sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # if button_play.check_input(mouse_pos_menu):
+                    #     pygame.mixer.music.stop()
+                    #     pygame.mixer.music.load("../logic/assets/audio/BGM_game.mp3")
+                    #     pygame.mixer.music.play(-1)
+                    #     run()
+                    if button_ldr.check_input(mouse_pos_menu):
+                        self.leaderboard()
+                    if button_credits.check_input(mouse_pos_menu):
+                        self.credit_screen()
+            pygame.display.update()
