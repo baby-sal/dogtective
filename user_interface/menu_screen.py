bf43a45a235b7d246cbf98776f3a2d8d5166b2fe
esterwalf <estellewalford@gmail.com>
2024-12-04 19:08:18 +0000

    moved menu_screen to user interface as file locations weren't copied correctly

--- user_interface/menu_screen.py
+++ user_interface/menu_screen.py
@@ -10,11 +10,11 @@ class MainMenu:
         pygame.init()
         self.display = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
         pygame.display.set_caption("Dogtective: Main Menu")
-        self.background = pygame.image.load("../../logic/assets/images/menu/city_backgroud.png").convert_alpha()
+        self.background = pygame.image.load("..logic/assets/images/menu/city_backgroud.png").convert_alpha()
         self.background = pygame.transform.smoothscale(self.background, self.display.get_size())
 
     def pixel_font(self, size):
-        return pygame.font.Font("../assets/images/StayPixelRegular.ttf", size)
+        return pygame.font.Font("../logic/assets/StayPixelRegular.ttf", size)
 
     def menu(self):
         while True:
