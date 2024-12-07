bd35f88ecd7f6bb49c0b815fff5f3856a2f15ba2
abbey <abbeygayle.p.dev@gmail.com>
2024-12-07 21:55:02 +0000

    unit testing and exception handling for score calculations

--- Testing/test_score.py
+++ Testing/test_score.py
@@ -1,17 +1,28 @@
 #unit test
-from unittest import TestCase,main
-
+from unittest import TestCase
+from logic.components.score import Score, NegativeTime, ZeroOrNegativeLives
 
 
 class TestScore(TestCase):
 
+#standard expectation of score calculation
     def test_score(self):
-        expected =
-        result =
+        expected = 1290
+        result = Score.calculate_score(1, 1)
         self.assertEqual(expected, result)
 
-    def test_negative_time(self):
-        expected =
-        result =
+#should the user take 5 minutes to perform achieve the end of the level
+    def test_extreme_long_time(self):
+        expected = 1000
+        result = Score.calculate_score(1, 300)
         self.assertEqual(expected, result)
+
+#if an error occurred and the time became negative
+    def test_negative_time(self):
+        with self.assertRaises(NegativeTime):
+            Score.calculate_score(1, -1)
+
+    def test_health_1_or_fewer(self):
+        with self.assertRaises(ZeroOrNegativeLives):
+            Score.calculate_score(0, 60)
 
