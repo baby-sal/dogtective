8c65488f1ea59592c4b50f2dd3cb5ed1b862fb97
z-for-zarrin <156834963+z-for-zarrin@users.noreply.github.com>
2024-12-07 16:32:13 +0000

    Revert "Revert "Timer and score skelly""

--- user_interface/game_runner.py
+++ user_interface/game_runner.py
@@ -1,25 +1,14 @@
-# from menu import
 import sys, pygame
+# from logic.score_db_connection.score_connection_to_saving_score_file import timer, health, score
 from user_interface.screens.game import GameLoop
 from user_interface.menu_runner import MenuRunner
 from user_interface.screens.credits import Credits
 from user_interface.screens.end_screen import EndScreen
 import user_interface.game_config as config
 from user_interface.screens.leaderboard import Leaderboard
-
+from logic.components.timer import Timer
 
 class Runner():
-
-    #while window == open:
-        #def run_game:
-            #while state = menu:
-                #menu
-            #if state = game
-            #decorator
-            #loop
-             #if teddy: return time
-                #score calc
-            #credits
 
     def __init__(self):
         pygame.init()
@@ -31,6 +20,11 @@ class Runner():
 
         self.current_state = config.GameState.MENU
 
+        self.timer = None
+        self.elapsed_time = 0
+        self.character = None
+
+
 
     def run(self):
         menu = MenuRunner(self.display, self)
@@ -39,13 +33,17 @@ class Runner():
         end_screen = EndScreen(self.display, self)
         leaderboard = Leaderboard(self.display, self)
 
+        self.character = game.dog
+
         game_on = True
 
         while game_on:
             if self.current_state == config.GameState.MENU:
                 menu.menu_runner()
             elif self.current_state == config.GameState.GAMEPLAY:
+                self.timer = Timer()
                 game.game_loop()
+                self.elapsed_time = self.timer.update_time()
             elif self.current_state == config.GameState.CREDITS:
                 credits.credit_screen()
             elif self.current_state == config.GameState.LEADERBOARD:
@@ -56,6 +54,9 @@ class Runner():
                 end_screen.you_lose()
             else:
                 game_on = False
+
+    def get_elapsed_time(self):
+        return self.elapsed_time
 
 
 if __name__ == "__main__":
