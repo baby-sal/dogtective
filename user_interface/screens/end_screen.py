ece659882fd301259f8193e86144047c5db9ab62
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-12-07 23:55:19 +0000

    changing positioning of play again, made clickable text red and other text purple

--- user_interface/screens/end_screen.py
+++ user_interface/screens/end_screen.py
@@ -11,6 +11,9 @@ class EndScreen(Screen):
         super().__init__(display, runner)
         self.db = DbClass()
 
+        self.button_go_back = Button(image=None, pos_x=1100, pos_y=50, font=self.text.pixel_font(40),
+                                colour="crimson", text_in="Menu")
+
     def you_win(self):
         health = self.runner.character.health.current
         elapsed_time = self.runner.get_elapsed_time()
@@ -23,29 +26,29 @@ class EndScreen(Screen):
             self.display.get_size())
         self.display.blit(bg, (0, 0))
 
-        self.text.text_blit("mission complete!", 160, "indigo", WIDTH / 2, 150)
-        self.text.number_blit(f"Score: {str(score)}", 100, "indigo", WIDTH / 2, 275)
-        self.text.number_blit(f"time: {elapsed_time} secs", 60, "crimson", WIDTH/2, 400)
-        self.text.number_blit(f"bones left: {health}", 60, "crimson", WIDTH/2, 450)
+        self.text.text_blit("mission complete!", 160, "indigo", WIDTH / 2, 175)
+        self.text.number_blit(f"Score: {str(score)}", 100, "indigo", WIDTH / 2, 300)
+        self.text.number_blit(f"time: {elapsed_time} secs", 60, "indigo", WIDTH/2, 400)
+        self.text.number_blit(f"bones left: {health}", 60, "indigo", WIDTH/2, 450)
         self.image.dogtective_image(5 * WIDTH / 6, 575, self.display)
 
-        button_go_back = Button(image=None, pos_x=1100, pos_y=50, font=self.text.pixel_font(40),
-                                colour="purple4", text_in="Menu")
+        button_play_again = Button(image=None, pos_x=WIDTH // 2, pos_y=600, font=self.text.pixel_font(50),
+                                   colour="crimson", text_in="Play again")
 
         while self.runner.current_state == GameState.WIN:
             mouse_pos_complete = pygame.mouse.get_pos()
 
-            button_go_back.update_button(self.display)
-            self.button_next_level.update_button(self.display)
+            self.button_go_back.update_button(self.display)
+            button_play_again.update_button(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if button_go_back.check_input(mouse_pos_complete):
+                    if self.button_go_back.check_input(mouse_pos_complete):
                         self.runner.current_state = GameState.MENU
-                    if self.button_next_level.check_input(mouse_pos_complete):
+                    if button_play_again.check_input(mouse_pos_complete):
                         pygame.mixer.music.stop()
                         pygame.mixer.music.load("../logic/assets/audio/BGM_game.mp3")
                         pygame.mixer.music.play(-1)
@@ -60,27 +63,26 @@ class EndScreen(Screen):
             self.display.get_size())
         self.display.blit(bg, (0, 0))
 
-        self.text.text_blit("GAME OVER", 200, "crimson", WIDTH / 2, 300)
+        self.text.text_blit("GAME OVER", 200, "indigo", WIDTH / 2, 200)
         self.image.dogtective_image(WIDTH / 2, 575, self.display)
-        self.button_play_again = Button(image=None, pos_x=WIDTH / 2, pos_y=500, font=self.text.pixel_font(75),
-                                      colour="crimson", text_in="Play Again")
-        button_go_back = Button(image=None, pos_x=1100, pos_y=50, font=self.text.pixel_font(40),
-                                    colour="purple4", text_in="Menu")
+
+        button_play_again = Button(image=None, pos_x=WIDTH // 2, pos_y=300, font=self.text.pixel_font(50),
+                                   colour="crimson", text_in="Play again")
 
         while self.runner.current_state == GameState.LOSE:
             mouse_pos_end = pygame.mouse.get_pos()
 
-            button_go_back.update_button(self.display)
-            self.button_play_again.update_button(self.display)
+            self.button_go_back.update_button(self.display)
+            button_play_again.update_button(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if button_go_back.check_input(mouse_pos_end):
+                    if self.button_go_back.check_input(mouse_pos_end):
                         self.runner.current_state = GameState.MENU
-                    if self.button_play_again.check_input(mouse_pos_play_again):
+                    if button_play_again.check_input(mouse_pos_end):
                         pygame.mixer.music.stop()
                         pygame.mixer.music.load("../logic/assets/audio/BGM_game.mp3")
                         pygame.mixer.music.play(-1)
