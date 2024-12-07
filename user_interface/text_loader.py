f3a28728a1cc6a0af7eef9cefe873b01d7158f5f
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-12-07 22:33:24 +0000

    added new font for numbers and created font directory in asset folder

--- user_interface/text_loader.py
+++ user_interface/text_loader.py
@@ -7,7 +7,7 @@ class Text():
         self.display = display
 
     def pixel_font(self, size):
-        return pygame.font.Font("../logic/assets/StayPixelRegular.ttf", size)
+        return pygame.font.Font("../logic/assets/fonts/StayPixelRegular.ttf", size)
 
     def text_blit(self, text, size, colour, rect_pos_x, rect_pos_y):
         text = self.pixel_font(size).render(text, True, colour)
