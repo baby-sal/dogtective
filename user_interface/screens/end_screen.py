e7532935fe95b99d07e16dd1ac7b376966bc9efb
esterwalf <estellewalford@gmail.com>
2024-12-07 22:10:44 +0000

    added game music

--- user_interface/screens/end_screen.py
+++ user_interface/screens/end_screen.py
@@ -1,7 +1,5 @@
 import pygame
 import sys
-
-
 from user_interface.screens.screen import Screen
 from user_interface.game_config import GameState, WIDTH
 from logic.components.button import Button
@@ -14,14 +12,12 @@ class EndScreen(Screen):
         self.db = DbClass()
 
     def you_win(self):
-
         health = self.runner.character.health.current
         elapsed_time = self.runner.get_elapsed_time()
         score = Score.calculate_score(health, elapsed_time)
         self.db.add_new_score(score)
         print(f"DB top ten: {self.db.get_top_ten()}")
         print(f"score: {score}")
-
 
         while self.runner.current_state == GameState.WIN:
             mouse_pos_complete = pygame.mouse.get_pos()
@@ -33,8 +29,10 @@ class EndScreen(Screen):
                 self.display.get_size())
             self.display.blit(bg, (0, 0))
 
-            self.text.text_blit("mission complete!", 160, "indigo", WIDTH/2, 300)
-            self.image.dogtective_image(WIDTH/2, 575, self.display)
+            self.text.text_blit("mission complete!", 160, "indigo", WIDTH / 2, 300)
+            self.text.text_blit(f"time spent: {elapsed_time} seconds", 60, "crimson", WIDTH / 2, 400)
+            self.text.text_blit(f"health remaining: {health}", 60, "crimson", WIDTH / 2, 450)
+            self.image.dogtective_image(WIDTH / 2, 575, self.display)
 
             button_go_back = Button(image=None, pos_x=1100, pos_y=50, font=self.text.pixel_font(40),
                                     colour="purple4", text_in="Menu")
@@ -67,13 +65,15 @@ class EndScreen(Screen):
                 self.display.get_size())
             self.display.blit(bg, (0, 0))
 
-            self.text.text_blit("GAME OVER", 200, "crimson", WIDTH/2, 300)
-            self.image.dogtective_image(WIDTH/2, 575, self.display)
+            self.text.text_blit("GAME OVER", 200, "crimson", WIDTH / 2, 300)
+            self.image.dogtective_image(WIDTH / 2, 575, self.display)
 
             button_go_back = Button(image=None, pos_x=1100, pos_y=50, font=self.text.pixel_font(40),
                                     colour="purple4", text_in="Menu")
 
             button_go_back.update_button(self.display)
+
+            self.button_play.update_button(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
