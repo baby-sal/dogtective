3f72fe272d2bc80d820552219681adef82e3ef5a
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-12-08 13:11:19 +0000

    static Text methods are now called statically

--- user_interface/text_loader.py
+++ user_interface/text_loader.py
@@ -15,11 +15,11 @@ class Text:
         return pygame.font.Font("../logic/assets/fonts/PixelGame.otf", size)
 
     def text_blit(self, text, size, colour, rect_pos_x, rect_pos_y):
-        text = self.pixel_font(size).render(text, True, colour)
+        text = Text.pixel_font(size).render(text, True, colour)
         rect = text.get_rect(center=(rect_pos_x, rect_pos_y))
         self.display.blit(text, rect)
 
     def number_blit(self, text, size, colour, rect_pos_x, rect_pos_y):
-        text = self.pixel_number_font(size).render(text, True, colour)
+        text = Text.pixel_number_font(size).render(text, True, colour)
         rect = text.get_rect(center=(rect_pos_x, rect_pos_y))
         self.display.blit(text, rect)
