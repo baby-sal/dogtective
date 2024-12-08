72a190d28f0f1f000ba85778e18bac8e4baa6176
z-for-zarrin <zarrin_r01@hotmail.co.uk>
2024-12-08 13:29:05 +0000

    refactoring Image class to mirror Text

--- user_interface/image_loader.py
+++ user_interface/image_loader.py
@@ -3,15 +3,16 @@ import pygame
 
 class Image:
 
-    @staticmethod
-    def dogtective_image(pos_x, pos_y, display):
+    def __init__(self, display):
+        self.display = display
+
+    def dogtective_image(self, pos_x, pos_y, display):
         dogtective_pic = pygame.image.load(
             "../logic/assets/images/characters/Dogtective_icon_no_background_1.png").convert_alpha()
         rect_dog = dogtective_pic.get_rect(center=(pos_x, pos_y))
-        display.blit(dogtective_pic, rect_dog)
+        self.display.blit(dogtective_pic, rect_dog)
 
-    @staticmethod
-    def dog_walk_image(pos_x, pos_y, display):
+    def dog_walk_image(self, pos_x, pos_y, display):
         smol_dog_pic = pygame.image.load("../logic/assets/images/characters/dogtective_sprite/Walk.png").convert_alpha()
         rect_dog = smol_dog_pic.get_rect(center=(pos_x, pos_y))
-        display.blit(smol_dog_pic, rect_dog)
+        self.display.blit(smol_dog_pic, rect_dog)
