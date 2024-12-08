a32872abbf7fbbe0e7ca81c9427887ca22ff5e40
Melanie Clark <melaniefclark@gmail.com>
2024-12-08 01:47:15 +0000

    image_loader.py static methods added to remove errors. Python formatting applied

--- user_interface/image_loader.py
+++ user_interface/image_loader.py
@@ -1,13 +1,17 @@
 import pygame
 
+
 class Image:
 
-    def dogtective_image(self, pos_x, pos_y, display):
-        dogtective_pic = pygame.image.load("../logic/assets/images/characters/Dogtective_icon_no_background_1.png").convert_alpha()
+    @staticmethod
+    def dogtective_image(pos_x, pos_y, display):
+        dogtective_pic = pygame.image.load(
+            "../logic/assets/images/characters/Dogtective_icon_no_background_1.png").convert_alpha()
         rect_dog = dogtective_pic.get_rect(center=(pos_x, pos_y))
         display.blit(dogtective_pic, rect_dog)
 
-    def dog_walk_image(self, pos_x, pos_y, display):
+    @staticmethod
+    def dog_walk_image(pos_x, pos_y, display):
         smol_dog_pic = pygame.image.load("../logic/assets/images/characters/dogtective_sprite/Walk.png").convert_alpha()
         rect_dog = smol_dog_pic.get_rect(center=(pos_x, pos_y))
         display.blit(smol_dog_pic, rect_dog)
